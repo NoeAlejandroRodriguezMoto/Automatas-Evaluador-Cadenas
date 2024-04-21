8812292294c83749348f16fe471a2af39d1b43da
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-20 22:46:59 -0600

    Refactorizando el script de evaluación de cadenas en Python3

--- evaluator_strings.py
+++ evaluator_strings.py
@@ -1,62 +1,57 @@
-# Noé Alejandro Rodríguez Moto - 221219 - Automata de cadenas - 7 - "A"
-
 import re
 
-DIGITO = "[0-9]"
-OPERADOR = "(\+|\-|\*|\/)"
-TABLA_TRANSICIONES = [[1,"E","E"],[1,2,"E"],[3,"E","E"],[3,2,"A"]]
-FIN = ""
+DIGIT = "[0-9]"
+OPERATOR = "(\+|\-|\*|\/)"
+TRANSITION_TABLE = [[1,"E","E"],[1,2,"E"],[3,"E","E"],[3,2,"A"]]
+END = ""
 
-def clasificar_caracter(character):
-    if(re.match(DIGITO, character)):
-        return 0, "Digito"
-    elif(re.match(OPERADOR, character)):
-        return 1, "Operador"
-    elif(character == FIN):
-        return 2, "FND"
+def classify_character(character):
+    if(re.match(DIGIT, character)):
+        return 0, "Digit"
+    elif(re.match(OPERATOR, character)):
+        return 1, "Operator"
+    elif(character == END):
+        return 2, "END"
     else:
-        print(f"Error el {character} no es valido")
+        print(f"Error, {character} is not valid")
         exit()
 
-def imprimir_encabezado():
-    print("|  Edo. Actual | Caracter |  Simbolo  |Edo. Siguiente |")
-    imprimir_linea()
+def print_header():
+    print("|  Current State | Character |  Symbol  |Next State |")
+    print_line()
 
-def imprimir_contenido(estadosig, character, simbolo, estado):
-    print("|     {0:2}       |    {1:1}     | {2:<7}   |     {3:2}       |".format(estadosig, character, simbolo, estado))
-    imprimir_linea()
+def print_content(next_state, character, symbol, state):
+    print("|     {0:2}       |    {1:1}     | {2:<7}   |     {3:2}       |".format(next_state, character, symbol, state))
+    print_line()
 
-def imprimir_linea():
+def print_line():
     print("+--------------+----------+-----------+---------------+")
 
-# MAIN
-estado = 0
+state = 0
 
 print ("""+---------------------------------------+
-|    Introduce una cadena a evaluar:    |
+|    Enter a string to evaluate:       |
 +---------------------------------------+""")
-cadena = input()
-imprimir_linea()
-imprimir_encabezado()
+string = input()
+print_line()
+print_header()
 
-for character in cadena:
-    estadosig = estado
-    charcaracter, simbolo = clasificar_caracter(character)
-    estado = TABLA_TRANSICIONES[estado][charcaracter]
+for character in string:
+    next_state = state
+    char_character, symbol = classify_character(character)
+    state = TRANSITION_TABLE[state][char_character]
 
-    if (estado == "E"):
-        imprimir_contenido(estadosig, character, simbolo, estado)
-        print("""|              Cadena No Valida                       |
+    if (state == "E"):
+        print_content(next_state, character, symbol, state)
+        print("""|              Invalid String                         |
 +-----------------------------------------------------+""")
         exit()
-    imprimir_contenido(estadosig, character, simbolo, estado)
+    print_content(next_state, character, symbol, state)
 
-if(estado != 3):
-    print("""|              Cadena No Valida                       |
+if(state != 3):
+    print("""|              Invalid String                         |
 +-----------------------------------------------------+""")
-elif(estado == 3):
-    imprimir_contenido(estado, ' ', 'FND', ' ')
-    print("""|                Cadena Valida                        |
+elif(state == 3):
+    print_content(state, ' ', 'END', ' ')
+    print("""|                Valid String                         |
 +-----------------------------------------------------+""")
-    
-# Cadenas probadas: validas: 12*3/4-5 y 6/3*2-1 no validas: 12++3 y 4/3--3
